ba26bc7402e0aabfc9c97968f82925d0c5ec19a2
Sarat Sista <sista.s@husky.neu.edu>
2014-11-26 19:49:36 -0500

    added code for cache.c, cache.h

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -1,31 +1,126 @@
+#include <string.h>
 #include "filesys/cache.h"
-
-/* Current size of the buffer cache */
-int static cur_cache_size;
+#include "filesys/filesys.h"
+#include "threads/malloc.h"
 
 void
 init_buffer_cache ()
 {
-  init_lock (&evict_lock);
-  init_list (&buffer_cache);
+  lock_init (&cache_lock);
+  list_init (&buffer_cache);
+
+  int i;
+  lock_acquire (&cache_lock);
+  for (i = 0; i < BUFFER_CACHE_SIZE; i++)
+   {
+     struct cache_entry *entry = allocate_cache_entry ();
+     list_push_front (&buffer_cache, &entry->elem);
+   }  
+  lock_release (&cache_lock);
 }
 
-struct cache_entry *
-allocate_cache_entry (block_sector_t sector, off_t bytes_covered)
+void
+write_cache (block_sector_t sector, int valid_bytes, void *buffer)
 {
-  if (cur_cache_size == BUFFER_CACHE_SIZE)
+  struct cache_entry *entry = find_cache_entry (sector, false); 
+  if (!entry)
    {
-     PANIC ("Buffer Cache Full!");
+     entry = find_cache_entry (sector, true);
+     if (!entry)
+      PANIC ("Disk Full");
+   }
+ 
+  memcpy (entry->data, buffer, valid_bytes);
+  entry->sector = sector;
+  entry->valid_bytes = valid_bytes;
+  entry->dirty = true;
+}
+
+void
+read_cache (block_sector_t sector, void *buffer)
+{
+  int zero_bytes;
+  struct cache_entry *entry = find_cache_entry (sector, false);
+  if (!entry)
+     entry = find_cache_entry (sector, true);
+
+  if (entry)
+   {
+     memcpy (buffer, entry->data, entry->valid_bytes);
+     zero_bytes = BLOCK_SECTOR_SIZE - entry->valid_bytes;
+     if (zero_bytes != 0)
+      {
+        memset (buffer + entry->valid_bytes, 0, zero_bytes);
+      }
    }
   else
    {
-     struct cache_entry *entry = calloc (1, sizeof (struct cache_entry));
-     entry->sector = sector;
-     entry->accessed = false;
-     entry->dirty = false;
-     entry->bytes_covered = bytes_covered;
-     lock_init (&entry->update_lock);
-     entry->is_growing = false;
-   }
+     PANIC ("Disk Full!");
+   }     
 }
 
+struct cache_entry *
+allocate_cache_entry ()
+{
+    struct cache_entry *entry = NULL;
+
+     entry = calloc (1, sizeof (struct cache_entry));
+     entry->sector = EMPTY;
+     entry->accessed = false;
+     entry->dirty = false;
+     entry->valid_bytes = EMPTY;
+     lock_init (&entry->update_lock);
+     entry->is_growing = false;
+     return entry;
+}
+
+/* Looks for a cache_entry in buffer_cache based on sector number.
+   If found, removes and pushes it to front of the list.
+   @param: sector_id - sector number of needed cache_entry
+           unused    - Do we need to find an unused cache_entry? */
+struct cache_entry *
+find_cache_entry (block_sector_t sector_id, bool unused)
+{
+  struct cache_entry *entry = NULL;
+  struct list_elem *e;
+  block_sector_t sector = (unused == true)? EMPTY:sector_id;
+
+  for (e = list_begin (&buffer_cache); e != list_end (&buffer_cache);
+       e = list_next (e))
+   {
+     entry = list_entry (e, struct cache_entry, elem);
+     if (entry->sector == (block_sector_t)sector)
+      {
+       list_remove (e);
+       list_push_front (&buffer_cache, &entry->elem);
+       return entry;
+      }
+   }
+   /* If an unused sector is requested and is not present in cache,
+      evict a cache_entry and return it */
+   if (sector == EMPTY)
+    {
+      lock_acquire (&cache_lock);
+      evict_cache_entry ();
+      entry = allocate_cache_entry ();
+      list_push_front (&buffer_cache, &entry->elem);
+      lock_release (&cache_lock);
+      return entry; 
+    } 
+  return NULL;   
+}
+
+bool
+evict_cache_entry ()
+{
+  struct list_elem *e = list_end (&buffer_cache);
+  struct cache_entry *entry = list_entry (e, struct cache_entry, elem);
+
+  if (entry->dirty)
+   {
+     block_write (fs_device, entry->sector, entry->data);
+   }
+  list_remove (e);
+  free (entry);
+  return true;
+}
--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -2,17 +2,18 @@
 #define FILESYS_CACHE_H
 
 #include <list.h>
+#include <limits.h>
 #include "devices/block.h"
 #include "threads/synch.h"
-#include "filesys/off_t.h"
 
 #define BUFFER_CACHE_SIZE 64
+#define EMPTY UINT_MAX 
 
 /* Buffer Cache: List of 64 cache entries */
 static struct list buffer_cache;
 
 /* Lock acquired while evicting an entry */
-struct lock evict_lock;
+struct lock cache_lock;
 
 /* Each cache entry in the buffer_cache */
 struct cache_entry
@@ -23,9 +24,8 @@ struct cache_entry
   bool dirty;			 /* Dirty bit for cache_entry */
   int open_count;		 /* Number of threads currently accessing 
 				    the entry */
-  off_t  bytes_covered;		 /* Number of bytes covered by this sector
-				    from the beginning of the file */ 
-  struct lock update_lock	 /* lock acquired when growing the sector
+  int  valid_bytes;		 /* Number of valid bytes in this sector*/
+  struct lock update_lock;	 /* lock acquired when growing the sector
      				    to set is_growing */
   bool is_growing;		 /* Boolean to indicate that file is growing */
   struct list_elem elem;
@@ -33,7 +33,9 @@ struct cache_entry
 
 void init_buffer_cache (void);
 void read_cache (block_sector_t, void *);
-void write_cache (block_sector_t, off_t, void *);
-struct cache_entry* allocate_cache_entry (block_sector_t, off_t);
+void write_cache (block_sector_t, int, void *);
+struct cache_entry* allocate_cache_entry (void);
+struct cache_entry* find_cache_entry (block_sector_t, bool);
+bool evict_cache_entry (void);
 
 #endif /* filesys/cache.h */
